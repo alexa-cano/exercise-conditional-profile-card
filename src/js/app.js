aea0e9394d41a8aa2f9453c5d2fa9bca65da77f7
alexa-cano <115123267+alexa-cano@users.noreply.github.com>
2025-07-09 02:38:21 +0000

    generador de perfil condicional

--- src/js/app.js
+++ src/js/app.js
@@ -23,27 +23,49 @@ import "../style/index.css";
     }
  */
 function render(variables = {}) {
-  console.log("These are the current variables: ", variables); // print on the console
-  // here we ask the logical questions to make decisions on how to build the html
-  // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
-  let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
-  if (variables.includeCover == false) cover = "<div class='cover'></div>";
+  console.log("These are the current variables: ", variables);
 
-  // reset the website body with the new html output
+  // Si includeCover es false, no mostramos imagen
+  let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
+  if (variables.includeCover === false) cover = `<div class="cover"></div>`;
+
+  // Valores por defecto si son null
+  let name = variables.name || "Name";
+  let lastName = variables.lastName || "Last Name";
+  let role = variables.role || "Role";
+  let city = variables.city || "City";
+  let country = variables.country || "Country";
+
+  // Redes sociales (solo si tienen valor)
+  let socialMedia = "";
+
+  if (variables.twitter) {
+    socialMedia += `<li><a href="https://twitter.com/${variables.twitter}"><i class="fab fa-twitter"></i></a></li>`;
+  }
+  if (variables.github) {
+    socialMedia += `<li><a href="https://github.com/${variables.github}"><i class="fab fa-github"></i></a></li>`;
+  }
+  if (variables.linkedin) {
+    socialMedia += `<li><a href="https://linkedin.com/in/${variables.linkedin}"><i class="fab fa-linkedin"></i></a></li>`;
+  }
+  if (variables.instagram) {
+    socialMedia += `<li><a href="https://instagram.com/${variables.instagram}"><i class="fab fa-instagram"></i></a></li>`;
+  }
+
+  // Posición de redes sociales
+  const positionClass = variables.socialMediaPosition || "position-right";
+
+  // Mostrar tarjeta
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
-            ${cover}
-          <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
-          </ul>
-        </div>
-    `;
+        ${cover}
+        <img src="${variables.avatarURL}" class="photo" />
+        <h1>${name} ${lastName}</h1>
+        <h2>${role}</h2>
+        <h3>${city}, ${country}</h3>
+        <ul class="${positionClass}">
+          ${socialMedia}
+        </ul>
+      </div>`;
 }
 
 /**
